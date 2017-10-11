090f4bcacc6448286d7772cc004f9f757bddd5bc
Thienthai <surkessone@gmail.com>
2017-10-12 04:08:51 +0700

    reder make

--- src/com/muic/game/candy/Game.java
+++ src/com/muic/game/candy/Game.java
@@ -2,11 +2,22 @@ package com.muic.game.candy;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Random;
 
 public class Game extends Canvas implements Runnable {
 
     private boolean running = false;
     private Thread thread;
+    private BufferedImage image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+    private BufferedImage background = null;
+    BufferedImage fish = null;
+    private int x = 0;
+    private int[][] board = new int[7][6];
+    Random rand = new Random();
+    private boolean initRen = true;
 
     private synchronized void start(){
         if(running)
@@ -29,9 +40,76 @@ public class Game extends Canvas implements Runnable {
         System.exit(1);
     }
 
+    private void init(){
+        BufferedImageLoader loader = new BufferedImageLoader();
+        requestFocus();
+        try{
+            background = loader.loadImage("background.png");
+            fish = loader.loadImage("red.png");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        for(int i = 0;i < 7;i++){
+            for(int j = 0;j < 6;j++){
+                int  n = rand.nextInt(6) + 1;
+                board[i][j] = n;
+            }
+        }
+    }
+
+    private void render() throws IOException {
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        Candies candies = new Candies();
+        g.drawImage(background, 0, 0, this);
+        candies.render(board, g, this);
+        g.drawRect(x,0,30,30);
+        g.dispose();
+        bs.show();
+    }
+
     @Override
     public void run() {
+        init();
+        long lastTime = System.nanoTime();
+        final double amoutOfTicks = 10.0;
+        double ns = 1000000000 / amoutOfTicks;
+        double delta = 0;
+        int updates = 0;
+        int frames = 0;
+        long timer = System.currentTimeMillis();
+        while(running){
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if(delta >= 1){
+                tick();
+                updates++;
+                delta--;
+            }
+            try {
+                render();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            frames++;
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println(updates + " Ticks,Fps " + frames);
+                updates = 0;
+                frames = 0;
+            }
+        }
+        stop();
+    }
 
+    private void tick() {
+        x++;
     }
 
     public static void main(String args[]){
